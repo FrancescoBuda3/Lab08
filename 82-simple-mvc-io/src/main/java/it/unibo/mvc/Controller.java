49bd19faeec9d2619008c9793b8261ef9d94647a
Francesco Buda <francesco.buda3@studio.unibo.it>
2022-11-15 10:02:27 +0100

    cominciato l'es 2 a casa

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,25 @@
 package it.unibo.mvc;
 
+import java.io.File;
+
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+
+    public static String HOME = System.getProperty("user.home");
+    public static String SEP = System.getProperty("file.separator");
+
+    private File current;
+
+    public Controller(){
+        this.current = new File(HOME + SEP + "output.txt");
+    }
+
+
+    public void setCurrentFile (File f) {
+        this.current = f;
+    }
 
 }
